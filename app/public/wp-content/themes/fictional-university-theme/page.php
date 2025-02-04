40c93b96d4791572f006f5308e6c423d43153845
Erik Konner <erikkonner@gmail.com>
2025-02-04 14:58:52 -0700

    Dynamic links for child and parent pages

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -26,13 +26,35 @@ while(have_posts()) {
         <?php }
     ?>
 
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php 
+    $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+    ));
+
+    if($parent_id or $testArray) { ?>
+        <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($parent_id); ?>"><?php echo get_the_title($parent_id);?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+            <?php
+            if ($parent_id) {
+                $findChildOf = $parent_id;
+                
+            } else {
+                $findChildOf = get_the_ID();
+            }
+            wp_list_pages(array(
+                "title_li" => NULL,
+                'child_of' => $findChildOf
+
+            ));
+            ?>
+         
         </ul>
-      </div> -->
+      </div>
+
+   <?php
+}?>
+      
 
       <div class="generic-content">
         <?php the_content(); ?>
