8270305672a93bd1a2e8b838bdb46065e80833c7
Erik Konner <erikkonner@gmail.com>
2025-02-05 16:55:34 -0700

    moved into must use directory

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -14,18 +14,7 @@ function university_features()
     add_theme_support('title-tag');
 }
 
-function university_post_types()
-{
-    register_post_type('event', array(
-        'public' => true,
-        'menu_icon' => 'dashicons-calendar-alt',
-        'labels' => array(
-            'name' => "Events"
-        )
 
-    ));
-}
 
 add_action('wp_enqueue_scripts', 'university_files');
 add_action('after_setup_theme', 'university_features');
-add_action('init', 'university_post_types');
